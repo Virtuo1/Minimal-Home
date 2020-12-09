6cd97c9b2098f35056af8a39aa6b1dd8d231f35c
Virtuo1 <albin.andersson041@gmail.com>
2020-12-09 10:25:30 +0100

    Changed search script

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 function search() {
-    var search_value = document.getElementById('search-content').value
-    window.close()
-    window.open('https://duckduckgo.com/?q=' + search_value)
+    var search_value = document.getElementById('search-value').value
+    location = 'https://duckduckgo.com/?q=' + search_value
     console.log('https://duckduckgo.com/?q=' + search_value)
+    return false
 }
